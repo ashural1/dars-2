95ede225e518aa6ed961fb3de293ea21d7cd297b
ashural1 <ashuraliabdurakhmonov1@outlook.com>
2024-05-04 20:46:03 +0500

    dars-2

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function App() {
+  const [counter, setCounter] = useState(0);
+
+  const cars = [
+    "./assets/mercc (1).jpg",
+    "./assets/mercc (2).jpg",
+    "./assets/mercc (3).jpg",
+    "./assets/merscce.jpg",
+  ];
+
+  // for(let i = 0; i<cars.length; i++)
+  // console.log(img);
+
+  return (
+    <div>
+      {/* <h1>{counter}</h1> */}
+      <img
+        className="img"
+        src={cars[counter]}
+        alt="cars-model"
+        width={600}
+        height={300}
+      />
+      <div className="btn-wrapper">
+        <button
+          className="back"
+          onClick={() => counter > 0 && setCounter(counter - 1)}
+        >
+          Back
+        </button>
+        <button
+          className="next"
+          onClick={() => counter < cars.length - 1 && setCounter(counter + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
